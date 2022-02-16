f6e7a2438f85a2c12ed179ac61d0c71fa0d6192e
Cevdet Ayaz <cevdetayaz@gmail.com>
2022-02-16 22:34:58 +0300

    if else örnekleri yazıldı

---
+++ ifelseyapisi/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int time = DateTime.Now.Hour;
+
+if(time >= 6 && time < 11)
+{
+    Console.WriteLine("Günaydın");
+}
+else if(time <= 18)
+{
+    Console.WriteLine("iyi günler");
+}
+else{
+    Console.WriteLine("iyi geceler");
+}
+
+string sonuc = time <= 18 ? "iyi günler" : "İyi geceler";
+
+sonuc = time >= 6 && time<11 ? "günaydın" : time<=18 ? "iyi günler" : "iyi geceler";
+Console.WriteLine(sonuc);
