b5386a735f95ad25c8caf2533c0e34a0b811e057
Cevdet Ayaz <cevdetayaz@gmail.com>
2022-02-17 00:21:02 +0300

    foreach while örnekleri eklendi

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// while 
+// 1'den başlayarak konsoldan girilen sayıya kadar (sayı dahil) ortalam hesaplayıp konsola yazdıran program.
+
+Console.Write("Bir sayı giriniz: ");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+
+while (sayac <= sayi)
+{
+    toplam += sayac;
+    sayac++;
+
+    
+}
+Console.WriteLine(toplam/sayi);
+
+
+// 'a' dan 'z' ye kadar tüm harfleri yazdır.
+
+char character = 'a';
+
+while (character < 'z')
+{
+    Console.Write(character);
+    character++;
+}
+
+Console.WriteLine("****  Foreach  ****");
+
+string[] arabalar = {"BMW", "FORD", "TOYOTA", "NISSAN"};
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
+
