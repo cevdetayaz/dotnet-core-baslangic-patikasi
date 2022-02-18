a5c130415b1096ae931c35842524168de15f92b8
Cevdet Ayaz <cevdetayaz@gmail.com>
2022-02-18 16:07:17 +0300

    metot örnekleri yazıldı

---
+++ metotlar/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// erisim_belirteci geri_donustipi metor_adi(parametreListesi/arguman)
+
+// {
+//     komutlar;
+// }
+
+
+int a = 2;
+int b = 3;
+
+Console.WriteLine(a+b);
+
+int sonuc = Topla(a,b);
+Console.WriteLine(sonuc);
+
+Metotlar ornek = new Metotlar();
+ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+int sonuc2 = ornek.ArttıVeTopla(ref a,ref b);
+ornek.EkranaYazdir(Convert.ToString(a + b));
+ornek.EkranaYazdir(Convert.ToString(sonuc2));
+
+
+
+static int Topla(int deger1, int deger2)
+{
+    return (deger1 + deger2);
+}
+
+class Metotlar
+{
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public int ArttıVeTopla(ref int deger1, ref int deger2)
+    {
+        deger1+=1;
+        deger2+=1;
+
+        return deger1 + deger2;
+    }
+}
