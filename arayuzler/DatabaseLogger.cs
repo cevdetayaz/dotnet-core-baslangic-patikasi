ebd1f1146739b3bbcf94f878729f834ba1a1192e
Cevdet Ayaz <cevdetayaz@gmail.com>
2022-03-08 16:29:16 +0300

    interface konusu işlendi.

---
+++ arayuzler/DatabaseLogger.cs
@@ -0,0 +1,9 @@
+public class DatabaseLogger : ILogger
+{
+    public void WriteLog()
+    {
+        // throw new NotImplementedException();
+        Console.WriteLine("Database'e log yazar.");
+        
+    }
+}
---
+++ arayuzler/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+FileLogger fileLogger = new FileLogger();
+fileLogger.WriteLog();
+
+DatabaseLogger databaseLogger = new DatabaseLogger();
+databaseLogger.WriteLog();
+
+SmsLogger smsLogger = new(); // C# 9.0 ile gelen bir özellik. Class ismini tekrar yazmadan obje oluşturabilir.
+smsLogger.WriteLog();
+
+
+LogManager logManager = new LogManager(new FileLogger());
+logManager.WriteLog();
