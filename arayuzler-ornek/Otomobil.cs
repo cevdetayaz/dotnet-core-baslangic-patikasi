5007a45f334404caa6e0ee0cdc1a10d7f7d0484a
Cevdet Ayaz <cevdetayaz@gmail.com>
2022-03-08 17:44:03 +0300

    abstract class örnekleri eklendi.

---
+++ arayuzler-ornek/Otomobil.cs
@@ -0,0 +1,13 @@
+public abstract class Otomobil
+{
+    public int KacTekerlektenOlusur(){
+        return 4;
+    }
+    public virtual Renk StandartRengiNe(){
+        return Renk.Beyaz;
+    }
+
+    public abstract Marka HangiMarkaninAraci();
+
+    
+}
--- arayuzler-ornek/Program.cs
+++ arayuzler-ornek/Program.cs
@@ -1,17 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("********** INTERFACE **********");
 
 Focus focus = new Focus();
 Console.WriteLine(focus.HangiMarkaninAraci().ToString());
 Console.WriteLine(focus.KacTekerlektenOlusur().ToString());
 Console.WriteLine(focus.StandartRengiNe().ToString());
 
+Console.WriteLine("************");
 Civic civic = new Civic();
 Console.WriteLine(civic.HangiMarkaninAraci().ToString());
 Console.WriteLine(civic.KacTekerlektenOlusur().ToString());
 Console.WriteLine(civic.StandartRengiNe().ToString());
 
+Console.WriteLine("************");
 Corolla corolla = new Corolla();
+Console.WriteLine(corolla.HangiMarkaninAraci().ToString());
+Console.WriteLine(corolla.KacTekerlektenOlusur().ToString());
+Console.WriteLine(corolla.StandartRengiNe().ToString());
+
+Console.WriteLine("************ ABSTRACT ***********");
+NewFocus focus1 = new NewFocus();
+Console.WriteLine(focus.HangiMarkaninAraci().ToString());
+Console.WriteLine(focus.KacTekerlektenOlusur().ToString());
+Console.WriteLine(focus.StandartRengiNe().ToString());
+
+Console.WriteLine("************");
+NewCivic civic1 = new NewCivic();
+Console.WriteLine(civic.HangiMarkaninAraci().ToString());
+Console.WriteLine(civic.KacTekerlektenOlusur().ToString());
+Console.WriteLine(civic.StandartRengiNe().ToString());
+
+Console.WriteLine("************");
+NewCorolla corolla1 = new NewCorolla();
 Console.WriteLine(corolla.HangiMarkaninAraci().ToString());
 Console.WriteLine(corolla.KacTekerlektenOlusur().ToString());
 Console.WriteLine(corolla.StandartRengiNe().ToString());
